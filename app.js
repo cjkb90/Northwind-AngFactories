dd5e4ad460fb7852de8a38d4f69ab811d47488e9
Christopher Kennedy <cjkb90@users.noreply.github.com>
2016-03-28 22:28:42 -0400

    Folders set up completed

--- app.js
+++ app.js
@@ -1,2 +1,10 @@
 var express = require('express');
 var app = express();
+
+app.get('/',function(req,res){
+	res.send("Hi, you're home");
+});
+
+app.listen(3000, function(){
+	console.log("Listening at port 3000");
+});
